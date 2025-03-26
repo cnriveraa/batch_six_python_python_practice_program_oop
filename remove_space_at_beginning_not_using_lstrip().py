48f1b4c8162f61491451ce52bf8e03467885379c
cnriveraa <rivera.cn18@gmail.com>
2025-03-26 20:54:27 +0800

    pseudocode added

---
+++ remove_space_at_beginning_not_using_lstrip().py
@@ -0,0 +1,4 @@
+# Prog01. ltrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# ask user to input a string
+# remove space at the beginning of the string
