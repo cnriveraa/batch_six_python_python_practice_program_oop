359614cb333aaf77de83a0df50cdaa63869b2dad
cnriveraa <rivera.cn18@gmail.com>
2025-03-26 21:04:27 +0800

    ask user to input characters to be removed

--- remove_characters_at_beginning_not_using_removeprefix().py
+++ remove_characters_at_beginning_not_using_removeprefix().py
@@ -4,5 +4,7 @@
 string = input("Enter a string: ")
 
 # ask user to input the characters to be removed
+characters = input("characters to be removed: ")
+
 # check if the characters to be removed is at the beginning of the string
 # remove the characters at the beginning of the string
