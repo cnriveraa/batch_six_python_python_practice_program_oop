96c82fdc556c9c2a76b91ea1a0f8e46a6c110afb
cnriveraa <rivera.cn18@gmail.com>
2025-03-26 20:55:15 +0800

    ask user to input a string

--- remove_space_at_beginning_not_using_lstrip().py
+++ remove_space_at_beginning_not_using_lstrip().py
@@ -1,4 +1,6 @@
 # Prog01. ltrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # ask user to input a string
+string = input("Enter a string (add space at the beginning): ")
+
 # remove space at the beginning of the string
