0dd91ef52b82a0da14afc934c783c5e629d74eb3
cnriveraa <rivera.cn18@gmail.com>
2025-03-26 21:03:49 +0800

    ask user to input a string

--- remove_characters_at_beginning_not_using_removeprefix().py
+++ remove_characters_at_beginning_not_using_removeprefix().py
@@ -1,6 +1,8 @@
 # Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 
 # ask user to input a string
+string = input("Enter a string: ")
+
 # ask user to input the characters to be removed
 # check if the characters to be removed is at the beginning of the string
 # remove the characters at the beginning of the string
