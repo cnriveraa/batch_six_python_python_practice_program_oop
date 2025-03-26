aa124666698a114be70f16a6e71433d888e7ad8d
cnriveraa <rivera.cn18@gmail.com>
2025-03-26 21:05:37 +0800

    check if the characters to be removed is at the beginning of the string

--- remove_characters_at_beginning_not_using_removeprefix().py
+++ remove_characters_at_beginning_not_using_removeprefix().py
@@ -4,7 +4,8 @@
 string = input("Enter a string: ")
 
 # ask user to input the characters to be removed
-characters = input("characters to be removed: ")
+characters = input("Characters to be removed: ")
 
 # check if the characters to be removed is at the beginning of the string
-# remove the characters at the beginning of the string
+if string.startswith(characters):
+    # remove the characters at the beginning of the string
