031523f45d33daad7c75a0ff2b4662bfb6f1974e
cnriveraa <rivera.cn18@gmail.com>
2025-03-27 18:40:43 +0800

    pseudocode added

---
+++ check_if_string_end_part_matches_function_parameter_not_using_endswith().py
@@ -0,0 +1,6 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+# function that checks if the string end part matches the function parameter 
+# ask the user to input a string
+# ask the user to input an end part
+# call the function and print the result
