0b47d933cab46024e2a9327e68facf0c104642d5
cnriveraa <rivera.cn18@gmail.com>
2025-03-27 19:15:38 +0800

    add a function to reverse casing pf each characters of a string

--- reverse_casing_of_each_characters_not_using_swapcase().py
+++ reverse_casing_of_each_characters_not_using_swapcase().py
@@ -1,5 +1,14 @@
 # Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 
-# function to reverse the casing of each character of the string        
+# function to reverse the casing of each character of the string
+def reverse_casing_of_each_characters(string):
+    result =''
+    for char in string:
+        if char.isupper():
+            result += char.lower()
+        else:
+            result += char.upper()
+    return result
+ 
 # ask user to input a string
 # call the function and print the result
