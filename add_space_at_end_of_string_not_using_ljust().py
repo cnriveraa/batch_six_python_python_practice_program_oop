91b48638b534dd11b605e8d5d73da2a152ee9d8a
cnriveraa <rivera.cn18@gmail.com>
2025-03-27 18:54:19 +0800

    pseudocode added

---
+++ add_space_at_end_of_string_not_using_ljust().py
@@ -0,0 +1,6 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# define a function that accepts a string and a number as parameter
+# ask user to input a string
+# ask user to input a number
+# call the function and pass the string and number as parameter
