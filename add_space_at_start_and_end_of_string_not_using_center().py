9e8f495e41c12c16de66948b959c3e0be630f984
cnriveraa <rivera.cn18@gmail.com>
2025-03-27 19:03:35 +0800

    pseudocode added

---
+++ add_space_at_start_and_end_of_string_not_using_center().py
@@ -0,0 +1,6 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# function to add space at the beginning and end of the string
+# ask the user to enter a string
+# ask the user to enter the length of the string
+# call the function and print the result
