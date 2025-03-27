d16c5034a27f627e7967486654d26cb70a099552
cnriveraa <rivera.cn18@gmail.com>
2025-03-27 18:54:57 +0800

    define a function that accepts a string and a number as parameter

--- add_space_at_end_of_string_not_using_ljust().py
+++ add_space_at_end_of_string_not_using_ljust().py
@@ -1,6 +1,9 @@
 # Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 
 # define a function that accepts a string and a number as parameter
+def add_space_at_end_of_string(string, length):
+    return string + ' ' * (length - len(string))
+
 # ask user to input a string
 # ask user to input a number
 # call the function and pass the string and number as parameter
