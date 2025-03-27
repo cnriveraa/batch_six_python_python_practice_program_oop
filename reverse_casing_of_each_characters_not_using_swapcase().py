b8b69821038f87940893b7d4b8cf928b1c80c898
cnriveraa <rivera.cn18@gmail.com>
2025-03-27 19:13:03 +0800

    pseudocode added

---
+++ reverse_casing_of_each_characters_not_using_swapcase().py
@@ -0,0 +1,5 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# function to reverse the casing of each character of the string        
+# ask user to input a string
+# call the function and print the result
